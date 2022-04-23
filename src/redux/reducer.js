47e1a0585a771868411a0b7c5843c306d2aac7b4
Geoff <geoffkellync@gmail.com>
2022-04-22 21:58:44 -0400

    fixed editing app to be managed via redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,68 @@
+import { combineReducers } from 'redux';
+import * as types from './action-types';
+import applicationData from '../data/dummyData';
+
+
+
+function applications(appData = applicationData, action) {
+    switch (action.type) {
+        case types.GET_APPLICATION: {
+
+            return action.payload
+        }
+        case types.EDIT_FORM: {
+            const newApplications = applicationData.map(app => {
+                if (app.id === action.payload.id) {
+                    return action.payload
+                }
+                return app
+                })
+            console.log(newApplications)
+            return appData = newApplications
+        }
+        default: 
+            return appData
+    }
+}
+
+const initialForm = {
+        companyName: '',
+        companyPhone: '',
+        companyWeb: '',
+        jobTitle: '',
+        dateApplied: '',
+        jobDiscovery: '',
+        salaryInfo: '',
+        contactQues: '',
+        contactName: '',
+        contactPhone: '',
+        contactEmail: '',
+        contactPosition: '',
+        currentStatus: '',
+        otherNotes: '',
+}
+
+function form(formState = initialForm,action){
+    switch(action.type){
+        case types.HANDLE_FORM_CHANGES: {
+            const { name, value } = action.payload
+            return { ...formState, [name]: value}
+        }
+        case types.HANDLE_FORM_SUBMIT: {
+            const  newApplication  = action.payload
+            return  applicationData.push(newApplication)
+        }
+        case types.DELETE_APPLICATION: {
+            const id = action.payload
+            console.log(`Deleting ${id}`)
+            return applicationData.filter(app => app.id !== id)
+        }
+        default: 
+            return formState
+    }
+}
+
+export default combineReducers({
+    applications,
+    form
+})
